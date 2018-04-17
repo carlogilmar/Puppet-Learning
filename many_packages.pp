8098067e145395164e6e1d00f3b5f0963be5f452
Carlo Gilmar <carlogilmar12@gmail.com>
2018-04-16 23:47:20 -0500

    agregando ejemplo de instalación de muchas cosas

---
+++ many_packages.pp
@@ -0,0 +1,15 @@
+package{ 'tmux':
+        ensure => installed,
+}
+package{ 'git':
+        ensure => installed,
+}
+package{ 'htop':
+        ensure => installed,
+}
+package{ 'zsh':
+        ensure => installed,
+}
+package{ 'fish':
+        ensure => installed,
+}
